7919888f5e0165071fc4dee824974202ff6fe5d6
jianghuaangte <jianghangte@gmail.com>
2025-12-17 19:19:34 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -e
+
+# 默认值
+VPN_USERNAME="${VPN_USERNAME:-$1}"
+VPN_PASSWORD="${VPN_PASSWORD:-$2}"
+LAN_IP="${LAN_IP:-$3}"
+GW_LAN_IP="${GW_LAN_IP:-$4}"
+NET_INTERFACE="${NET_INTERFACE:-$5}"
+NGINX_ENABLE="${NGINX_ENABLE:-0}"
+
+
+# 建立 VPN 连接
+connect_vpn() {
+    # 建立连接
+    openvpn --config client.ovpn --auth-user-pass user-passwd.txt &
+}
+
+# 路由表
+ip_routes() {
+    ip route add $VPN_SERVER via $GW_LAN_IP dev $NET_INTERFACE metric 100
+    ip route add $LAN_IP via $GW_LAN_IP dev eth0 metric 70
+    ip route add default dev ppp0 metric 50
+    ip route del default via $GW_LAN_IP dev $NET_INTERFACE
+}
+
+# Nginx
+start_nginx_if_enabled() {
+  if [ "$NGINX_ENABLE" = "1" ]; then
+    nginx
+  fi
+}
+
+
+# 主函数
+main() {
+    # 建立连接
+    connect_vpn
+    # 路由
+    ip_routes
+    # Nginx
+    start_nginx_if_enabled
+    # 保持容器运行
+    tail -f /dev/null
+}
+
+# 运行主函数
+main
